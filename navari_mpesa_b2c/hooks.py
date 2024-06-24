b505c6359ebf1fca0e12298f7ff63321c8a1ae4e
GichanaMayaka <gichanamayaka@gmail.com>
2024-06-24 10:51:59 +0300

    style: minor refactors to hooks.py file.

--- navari_mpesa_b2c/hooks.py
+++ navari_mpesa_b2c/hooks.py
@@ -46,7 +46,7 @@ required_apps = ["frappe/erpnext/hrms/payments"]
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"Role": "home_page"
 # }
 
 # Generators
@@ -60,8 +60,8 @@ required_apps = ["frappe/erpnext/hrms/payments"]
 
 # add methods and filters to jinja environment
 # jinja = {
-#	"methods": "navari_mpesa_b2c.utils.jinja_methods",
-#	"filters": "navari_mpesa_b2c.utils.jinja_filters"
+# 	"methods": "navari_mpesa_b2c.utils.jinja_methods",
+# 	"filters": "navari_mpesa_b2c.utils.jinja_filters"
 # }
 
 # Installation
@@ -103,11 +103,11 @@ required_apps = ["frappe/erpnext/hrms/payments"]
 # Permissions evaluated in scripted ways
 
 # permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
 # }
 #
 # has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
 # DocType Class
@@ -115,7 +115,7 @@ required_apps = ["frappe/erpnext/hrms/payments"]
 # Override standard doctype classes
 
 # override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
+# 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
 
 # Document Events
@@ -123,32 +123,32 @@ required_apps = ["frappe/erpnext/hrms/payments"]
 # Hook on document methods and events
 
 # doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
 # }
 
 # Scheduled Tasks
 # ---------------
 
 # scheduler_events = {
-#	"all": [
-#		"navari_mpesa_b2c.tasks.all"
-#	],
-#	"daily": [
-#		"navari_mpesa_b2c.tasks.daily"
-#	],
-#	"hourly": [
-#		"navari_mpesa_b2c.tasks.hourly"
-#	],
-#	"weekly": [
-#		"navari_mpesa_b2c.tasks.weekly"
-#	],
-#	"monthly": [
-#		"navari_mpesa_b2c.tasks.monthly"
-#	],
+# 	"all": [
+# 		"navari_mpesa_b2c.tasks.all"
+# 	],
+# 	"daily": [
+# 		"navari_mpesa_b2c.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"navari_mpesa_b2c.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"navari_mpesa_b2c.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"navari_mpesa_b2c.tasks.monthly"
+# 	],
 # }
 
 # Testing
@@ -160,14 +160,14 @@ required_apps = ["frappe/erpnext/hrms/payments"]
 # ------------------------------
 #
 # override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "navari_mpesa_b2c.event.get_events"
+# 	"frappe.desk.doctype.event.event.get_events": "navari_mpesa_b2c.event.get_events"
 # }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
 # override_doctype_dashboards = {
-#	"Task": "navari_mpesa_b2c.task.get_dashboard_data"
+# 	"Task": "navari_mpesa_b2c.task.get_dashboard_data"
 # }
 
 # exempt linked doctypes from being automatically cancelled
@@ -193,29 +193,29 @@ required_apps = ["frappe/erpnext/hrms/payments"]
 # --------------------
 
 # user_data_fields = [
-#	{
-#		"doctype": "{doctype_1}",
-#		"filter_by": "{filter_by}",
-#		"redact_fields": ["{field_1}", "{field_2}"],
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_2}",
-#		"filter_by": "{filter_by}",
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_3}",
-#		"strict": False,
-#	},
-#	{
-#		"doctype": "{doctype_4}"
-#	}
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
 # ]
 
 # Authentication and authorization
 # --------------------------------
 
 # auth_hooks = [
-#	"navari_mpesa_b2c.auth.validate"
+# 	"navari_mpesa_b2c.auth.validate"
 # ]
