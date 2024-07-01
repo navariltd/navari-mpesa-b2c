ddaa720f0401b7ffc4171094eb5bfc58b72ad744
GichanaMayaka <gichanamayaka@gmail.com>
2024-07-01 16:43:54 +0300

    chore: added to_dict() method to B2CRequestDefinition dataclass.
    This is to quickly get a dict representation of json data, excluding a few fields, precluding the need to manually build the payload when needed.

--- navari_mpesa_b2c/mpesa_b2c/utils/definitions.py
+++ navari_mpesa_b2c/mpesa_b2c/utils/definitions.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 
 @dataclass(init=True, frozen=True)
@@ -15,3 +15,15 @@ class B2CRequestDefinition:
     PartyB: str
     Remarks: str
     Occassion: str
+
+    def to_dict(self) -> dict:
+        """Returns a Dictionary representation of the Dataclass, excluding fields Setting, 
+        ConsumerKey, and ConsumerSecret
+
+        Returns:
+            dict: The dictionary representation
+        """
+        data_dict = asdict(self)
+        exclude_fields = {"Setting", "ConsumerKey", "ConsumerSecret"}
+        filtered_dict = {k: v for k, v in data_dict.items() if k not in exclude_fields}
+        return filtered_dict
