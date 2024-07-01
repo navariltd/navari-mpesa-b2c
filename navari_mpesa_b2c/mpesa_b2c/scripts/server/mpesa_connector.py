d8f2fb08bc90313937d1039ee5042fde58090071
wafula <wafulaat@gmail.com>
2024-07-01 12:33:14 +0300

    chore: Initiate comm with daraja

--- navari_mpesa_b2c/mpesa_b2c/scripts/server/mpesa_connector.py
+++ navari_mpesa_b2c/mpesa_b2c/scripts/server/mpesa_connector.py
@@ -3,11 +3,15 @@ from enum import Enum
 
 import frappe
 import requests
+import json
 from requests.auth import HTTPBasicAuth
 
 from ...utils.definitions import B2CRequestDefinition
-from ...utils.doctype_names import DARAJA_ACCESS_TOKENS_DOCTYPE
+from ...utils.doctype_names import DARAJA_ACCESS_TOKENS_DOCTYPE, MPESA_B2C_PAYMENT_DOCTYPE
 from ...utils.helpers import save_access_token
+from frappe.utils.password import get_decrypted_password
+from frappe.utils import get_request_site_address
+from frappe.integrations.utils import create_request_log
 
 
 class URLS(Enum):
@@ -101,4 +105,73 @@ class MpesaB2CConnector:
             ]
 
         else:
-            pass
+            hashed_token = token[0]
+
+            bearer_token = get_decrypted_password(DARAJA_ACCESS_TOKENS_DOCTYPE, hashed_token, 'access_token')
+
+            saf_url = "{}{}".format(self.base_url,'/mpesa/b2c/v3/paymentrequest')
+
+            callback_url = (get_request_site_address()
+                            + "/api/method/navari_mpesa_b2c.mpesa_b2c.scripts.server.mpesa_connector.results_callback_url")
+
+            payload = {
+                "OriginatorConversationID": request_data.OriginatorConversationID,
+                "InitiatorName": request_data.InitiatorName,
+                "SecurityCredential": request_data.SecurityCredential,
+                "CommandID": request_data.CommandID,
+                "Amount": request_data.Amount,
+                "PartyA": request_data.PartyA,
+                "PartyB": request_data.PartyB,
+                "Remarks": request_data.Remarks,
+                "QueueTimeOutURL":"https://example.com",
+                "ResultURL": callback_url,
+                "Occassion": request_data.Occassion
+            }
+
+            payload = json.dumps(payload)
+            try:
+                response = requests.post(
+                    saf_url,
+                    data=payload,
+                    headers={
+                    "Authorization": f"Bearer {bearer_token}",
+                    "Content-Type": "application/json",
+                    },
+                    timeout=60
+                )
+                
+                response.raise_for_status()
+                    
+            except requests.HTTPError:
+                frappe.throw("Exception encountered when sending payment request")
+
+            except requests.ConnectionError:
+                frappe.throw("Exception encountered when sending payment request")
+    
+            except Exception as e:
+                frappe.throw("An error occurred")
+
+            frappe.msgprint('Payment Request accepted for processing', title="Successful", indicator="green" )
+
+            response_dict = json.loads(response.text)
+
+            response = frappe._dict(response_dict)
+
+             # Use ConversationID as the request name
+            if getattr(response, "ConversationID"):
+                req_name = getattr(response, "ConversationID", None)
+                error = None
+            else:
+                # Check error response
+                req_name = getattr(response, "requestId")
+                error = response
+
+            if not frappe.db.exists("Integration Request", req_name):
+                create_request_log(payload, "Host", "Mpesa", req_name, error)
+                
+            return response
+        
+@frappe.whitelist(allow_guest=True)
+def results_callback_url(**kwargs) -> None:
+    frappe.msg_print("Callback received")
+    
