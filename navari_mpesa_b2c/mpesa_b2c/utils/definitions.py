f8463efbbbe265e56ab29b98f02fee4887dea9db
GichanaMayaka <gichanamayaka@gmail.com>
2024-07-02 09:50:20 +0300

    refactor: minor refactor

--- navari_mpesa_b2c/mpesa_b2c/utils/definitions.py
+++ navari_mpesa_b2c/mpesa_b2c/utils/definitions.py
@@ -19,12 +19,12 @@ class B2CRequestDefinition:
     Remarks: str
     Occassion: str
 
-    def to_dict(self, with_kwargs: dict[str, str] | None = None) -> dict:
+    def to_dict(self, with_dict: dict[str, str] | None = None) -> dict:
         """Returns a Dictionary representation of the Dataclass, excluding fields Setting,
         ConsumerKey, and ConsumerSecret
 
         Args:
-            with_kwargs (dict[str, str] | None): Additional dict like object to add to the
+            with_dict (dict[str, str] | None): Additional dict like object to add to the
             resultant dictionary
 
         Returns:
@@ -34,16 +34,20 @@ class B2CRequestDefinition:
         exclude_fields = {"Setting", "ConsumerKey", "ConsumerSecret"}
         filtered_dict = {k: v for k, v in data_dict.items() if k not in exclude_fields}
 
-        if isinstance(with_kwargs, dict):
-            filtered_dict.update(with_kwargs)
+        if isinstance(with_dict, dict):
+            filtered_dict.update(with_dict)
 
         return filtered_dict
 
-    def to_json(self, with_kwargs: dict[str, str] | None = None) -> str:
+    def to_json(self, with_dict: dict[str, str] | None = None) -> str:
         """Returns a JSON representation of the dataclass, excluding fields Setting,
         ConsumerKey, and ConsumerSecret
+
+        Args:
+            with_dict (dict[str, str] | None): Additional dict like object to add to the
+            resultant JSON string
 
         Returns:
             str: The JSON representation of the dataclass values
         """
-        return json.dumps(self.to_dict(with_kwargs))
+        return json.dumps(self.to_dict(with_dict))
