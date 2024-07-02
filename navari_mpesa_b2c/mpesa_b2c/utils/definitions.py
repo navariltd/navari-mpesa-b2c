40ff7d5c9dd0d08c1a3c7cb4aa498938efce5de8
GichanaMayaka <gichanamayaka@gmail.com>
2024-07-02 09:46:29 +0300

    chore: enhanced the B2CRequestDefinition class with the to_json() method allowing for dynamic building of the payload.

--- navari_mpesa_b2c/mpesa_b2c/utils/definitions.py
+++ navari_mpesa_b2c/mpesa_b2c/utils/definitions.py
@@ -1,8 +1,11 @@
+import json
 from dataclasses import asdict, dataclass
 
 
 @dataclass(init=True, frozen=True)
 class B2CRequestDefinition:
+    """Dataclass bearing the Request Data sent to Daraja API"""
+
     Setting: str
     ConsumerKey: str
     ConsumerSecret: str
@@ -16,14 +19,31 @@ class B2CRequestDefinition:
     Remarks: str
     Occassion: str
 
-    def to_dict(self) -> dict:
-        """Returns a Dictionary representation of the Dataclass, excluding fields Setting, 
+    def to_dict(self, with_kwargs: dict[str, str] | None = None) -> dict:
+        """Returns a Dictionary representation of the Dataclass, excluding fields Setting,
         ConsumerKey, and ConsumerSecret
 
+        Args:
+            with_kwargs (dict[str, str] | None): Additional dict like object to add to the
+            resultant dictionary
+
         Returns:
-            dict: The dictionary representation
+            dict: The dictionary representation of the dataclass
         """
         data_dict = asdict(self)
         exclude_fields = {"Setting", "ConsumerKey", "ConsumerSecret"}
         filtered_dict = {k: v for k, v in data_dict.items() if k not in exclude_fields}
+
+        if isinstance(with_kwargs, dict):
+            filtered_dict.update(with_kwargs)
+
         return filtered_dict
+
+    def to_json(self, with_kwargs: dict[str, str] | None = None) -> str:
+        """Returns a JSON representation of the dataclass, excluding fields Setting,
+        ConsumerKey, and ConsumerSecret
+
+        Returns:
+            str: The JSON representation of the dataclass values
+        """
+        return json.dumps(self.to_dict(with_kwargs))
